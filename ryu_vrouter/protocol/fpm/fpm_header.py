811a491e99c3d4008da77d6d213924864533e409
Yi Tseng <a86487817@gmail.com>
2016-07-22 18:40:55 +0800

    Add fpm header parser

---
+++ ryu_vrouter/protocol/fpm/fpm_header.py
@@ -0,0 +1,44 @@
+import struct
+from ryu.lib.packet import packet_base
+from ryu.lib import addrconv
+
+from .netlink import Netlink
+
+class FpmHeader(packet_base.PacketBase):
+    """
+    FPM header encoder/decoder class
+
+    An instance has the following attributes at least.
+
+    __init__ takes the corresponding args in this order.
+
+    =============== ==================== =====================
+    Attribute       Description          Example
+    =============== ==================== =====================
+    version
+    msg_type
+    length
+    =============== ==================== =====================
+    """
+
+    _PACK_STR = '!HHI'
+    _MIN_LEN = struct.calcsize(_PACK_STR)
+
+    def __init__(self, version, msg_type, length):
+        super(Netlink, self).__init__()
+        self.version = version
+        self.msg_type = msg_type
+        self.length = length
+
+    @classmethod
+    def parser(cls, buf):
+        version, msg_type, length = struct.unpack_from(cls._PACK_STR, buf)
+        return (cls(version, msg_type, length),
+                Netlink,
+                buf[FpmHeader._MIN_LEN:])
+
+    def serialize(self, payload, prev):
+        '''
+        Not implement now
+        '''
+        return None
